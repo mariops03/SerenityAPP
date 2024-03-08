83e07095245e59c83a1f7cf669b55d490cfddfff
Mario <marioprieta@gmail.com>
2024-03-08 14:36:10 +0100

    Cambio a vue

---
+++ web/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>MiSitio</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list>
+        <v-list-item link :to="routes.home">
+          <v-list-item-title>Inicio</v-list-item-title>
+        </v-list-item>
+        <v-list-item link :to="routes.about">
+          <v-list-item-title>Acerca</v-list-item-title>
+        </v-list-item>
+        <v-list-item link :to="routes.contact">
+          <v-list-item-title>Contacto</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const drawer = ref(false);
+const routes = {
+  home: '/',
+  about: '/acerca',
+  contact: '/contacto'
+};
+</script>
---
+++ web/src/router/router.js
@@ -0,0 +1,18 @@
+// router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '../App.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: App
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
