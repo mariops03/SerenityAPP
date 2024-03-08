f9a843a36b19a3069bf2a9c7017622a92e6a781f
Mario Prieta Sánchez <145470491+mariops03@users.noreply.github.com>
2024-03-08 14:51:46 +0100

    Refactor Navbar.vue component to remove navigation drawer

--- web/src/components/Navbar.vue
+++ web/src/components/Navbar.vue
@@ -1,30 +1,18 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>MiSitio</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text :to="routes.home">Inicio</v-btn>
+      <v-btn text :to="routes.about">Acerca</v-btn>
+      <v-btn text :to="routes.contact">Contacto</v-btn>
     </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" app temporary>
-      <v-list>
-        <v-list-item link :to="routes.home">
-          <v-list-item-title>Inicio</v-list-item-title>
-        </v-list-item>
-        <v-list-item link :to="routes.about">
-          <v-list-item-title>Acerca</v-list-item-title>
-        </v-list-item>
-        <v-list-item link :to="routes.contact">
-          <v-list-item-title>Contacto</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
   </v-app>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-const drawer = ref(false);
 const routes = {
   home: '/',
   about: '/acerca',
