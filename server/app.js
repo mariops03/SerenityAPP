06f15047cebd114639e91f7f16cafd16d38e985d
Dani Mulas <danimulas28@gmail.com>
2024-05-06 23:08:48 +0200

    .

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
 app.use(cors({
   //origin: 'http://localhost:5173',
   
-  origin: '*',
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
