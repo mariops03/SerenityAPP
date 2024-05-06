2f7fbd83f121daadd3b71293800d1ee9bceded9d
Dani Mulas <danimulas28@gmail.com>
2024-05-06 23:06:40 +0200

    cors

--- server/app.js
+++ server/app.js
@@ -7,9 +7,20 @@ const app = express();
 const port = 3000;
 
 require('./config/db');
-
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', req.headers.origin); //solve error
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Set-Cookie');
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 app.use(cors({
   //origin: 'http://localhost:5173',
+  
   origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
